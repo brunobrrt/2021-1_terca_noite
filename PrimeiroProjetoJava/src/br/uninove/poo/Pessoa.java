7050673a9a1dfa1919c78b4fb3787446044a03b1
Thiago G. Traue <t.traue@gmail.com>
2021-03-09 21:58:23 -0300

    Update da Aula 1

---
+++ PrimeiroProjetoJava/src/br/uninove/poo/Pessoa.java
@@ -0,0 +1,8 @@
+package br.uninove.poo;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    
+    //Paramos aqui. Começar: Construtores e encapsulamento 
+}
--- PrimeiroProjetoJava/src/br/uninove/poo/Principal.java
+++ PrimeiroProjetoJava/src/br/uninove/poo/Principal.java
@@ -1,7 +1,32 @@
 package br.uninove.poo;
 
 public class Principal {
+    
     public static void main(String[] args) {
-        System.out.println("Olá galera da Uninove!");
+        //System.out.println("Palavra \"1\"");
+        //System.out.println("Palavra \\ \"2\"");
+        
+        //Declaração de variáveis:
+//        int x;
+//        boolean y = true;
+//        char c = 'S';
+//        float numeroReal;
+//        long longo;
+//        double d;
+//        String texto = "Oi eu sou um texto";
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Josefino";
+        p1.idade = 33;
+       
+        System.out.println("Olá " + p1.nome);
+        System.out.println("Vi que você tem " + p1.idade + " anos! Da hora!");
+        
     }
 }
+
+/*
+TODO objeto tem três coisas: 
+ - Nome, --> Nome da classe
+ - Carcterístias, --> Atributos
+ - Comportmanetos --> Métodos
+*/
